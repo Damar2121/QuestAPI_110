42e4701823d930b21c121a10d0639efec6b69917
Damar2121 <damar.vivoy51@gmail.com>
2025-12-18 18:02:37 +0700

    memngcoding untuk PenyediaViewModel.kt

--- app/src/main/java/com/example/pertemuan12/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,24 @@
-package com.example.pertemuan12.viewmodel.provider
+package pertemuan12.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import pertemuan12.repositori.AplikasiDataSiswa
+import pertemuan12.viewmodel.EntryViewModel
+import pertemuan12.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+    }
+}

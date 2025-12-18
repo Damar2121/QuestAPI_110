fe4bba19f1f86f7b2811f2f553a4d28da91a7302
Damar2121 <damar.vivoy51@gmail.com>
2025-12-18 17:34:21 +0700

    memngcoding untuk DestinasiHome

--- app/src/main/java/com/example/pertemuan12/uiController/route/DestinasiHome.kt
+++ app/src/main/java/com/example/pertemuan12/uiController/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
-package com.example.pertemuan12.uiController.route
+package pertemuan12.uicontroller.route
 
-object DestinasiHome {
+import com.example.mydatasisw.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
